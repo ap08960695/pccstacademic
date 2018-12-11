f03e3842bdf3602c500e1da42cd12f4514bf180b
Phaisit-Big <Phaisit.Si@tnis.com>
2018-12-11 23:10:11 +0700

    [ADD] can edit information school

---
+++ admin/db_edit_school.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+	date_default_timezone_set('Asia/Bangkok');
+    include_once('../condb.php');
+    include_once('admin_check.php');
+    
+	$sql = "UPDATE school SET display='".$_POST['school_display']."',email='".$_POST['school_email']."',amper='".$_POST['school_amper']."',changwat='".$_POST['school_changwat']."',addrcode='".$_POST['school_zip']."',phone='".$_POST['school_phone']."'";
+	$result = mysql_query($sql ,$conn);
+	if(!$result){
+		mysql_close($conn);
+		header("location:edit_school.php?act=error_edit");
+		exit();
+	}else{
+		mysql_close($conn);
+		header("location:index.php?act=success_edit_school");
+		exit();	
+	}
+?>
