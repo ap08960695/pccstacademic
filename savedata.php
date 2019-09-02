a3b77eaf6e210a9521b90cb3341643054ac16c50
Phaisit-Big <Phaisit.Si@tnis.com>
2019-09-02 15:09:31 +0700

    edit error string in sql

--- savedata.php
+++ savedata.php
@@ -8,7 +8,6 @@ $schoolname = $school_info["display"];
 
 $arrstudent = $_POST['student'];
 $arrteacher = $_POST['teacher'];
-$user = $_SESSION["usr"];
 
 
 $sql = "UPDATE register SET status=0 WHERE running_year = '$running_year' AND school_id='$school_code'";
@@ -18,27 +17,27 @@ $len_name = 0;
 foreach ($arrstudent as $key => $value) {
 	foreach ($value as $sub_value) {
 		if (preg_replace('/[ -]/', '', $sub_value) != "") {
-			$sql_string .= "('$school_code', $key,'$sub_value','$running_year'),";
+			$sql_string .= "('$school_code', $key,'$sub_value',-1,'$running_year'),";
 			$list_name[$sub_value] = 1;
 			$len_name++;
 		} else if ($_POST['role'] == "edit") {
-			$sql_string .= "('$school_code', $key,'$sub_value',-1,'$running_year'),";
+			$sql_string .= "('$school_code', $key,'$sub_value','$running_year'),";
 		}
 	}
 }
 if ($len_name != count($list_name)) {
 	$sql = "UPDATE register SET status=1 WHERE running_year = '$running_year' AND school_id='$school_code'";
 	mysqli_query($conn, $sql);
-	header("location:index.php?error=update_student_same");
+	header("location:user_register.php?error=update_student_same");
 	exit();
 } else if ($sql_string != "") {
 	$sql_string = substr($sql_string, 0, -1);
-	$sql = "INSERT INTO `register` (`school_id`, `subject_id`, `name`, `score`,'running_year') VALUES $sql_string;";
+	$sql = "INSERT INTO `register` (`school_id`, `subject_id`, `name`, `score`,`running_year`) VALUES $sql_string;";
 	$result = mysqli_query($conn, $sql);
 	if (!$result) {
 		$sql = "UPDATE register SET status=1 WHERE running_year = '$running_year' AND school_id='$school_code'";
 		mysqli_query($conn, $sql);
-		header("location:index.php?error=update_student");
+		header("location:user_register.php?error=update_student");
 		exit();
 	} else {
 		$sql = "DELETE FROM register WHERE running_year = '$running_year' AND school_id='$school_code' AND status=0";
@@ -58,18 +57,17 @@ foreach ($arrteacher as $key => $value) {
 }
 if ($sql_string != "") {
 	$sql_string = substr($sql_string, 0, -1);
-	$sql = "INSERT INTO `register_teacher` (`school_id`, `subject_id`, `name`,'running_year') VALUES $sql_string;";
+	$sql = "INSERT INTO `register_teacher` (`school_id`, `subject_id`, `name`,`running_year`) VALUES $sql_string;";
 	$result = mysqli_query($conn, $sql);
 	if (!$result) {
 		$sql = "UPDATE register_teacher SET status=1 WHERE running_year = '$running_year' AND school_id='$school_code'";
 		mysqli_query($conn, $sql);
-		header("location:index.php?error=update_teacher");
+		header("location:user_register.php?error=update_teacher");
 		exit();
 	} else {
 		$sql = "DELETE FROM register_teacher WHERE running_year = '$running_year' AND school_id='$school_code' AND status=0";
 		mysqli_query($conn, $sql);
 	}
 }
-
 mysqli_close($conn);
-header("location:index.php?success=true");
+header("location:user_register.php?success=true");
